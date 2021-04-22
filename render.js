40a806a0dffe38f983d769d3854b030a8ba5ce9d
Kshitij <kshitijg9899@gmail.com>
2021-04-22 19:03:19 +0530

    styling + address of cell

--- render.js
+++ render.js
@@ -1,1 +1,16 @@
 // alert("Hello");
+// cheerio se elements are selected -> it is based on jquery -> helps in change of dom
+
+$(document).ready(function() {
+    // these are representations of the element
+    // let allCells = $(".grid .row .cell");
+    // console.log(allCells);
+
+    // jquery ne events diye huye hain
+    $(".grid .row .cell").on("click", function() {
+        // console.log("cell was clicked");
+        let clickedCell = this;
+        let cellIdx = $(clickedCell).text();
+        console.log(cellIdx);
+    })
+})
