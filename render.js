59024549d736c17584b852647558bd9da939afb5
Kshitij <kshitijg9899@gmail.com>
2021-04-24 01:08:27 +0530

    address bar

--- render.js
+++ render.js
@@ -10,7 +10,18 @@ $(document).ready(function() {
     $(".grid .row .cell").on("click", function() {
         // console.log("cell was clicked");
         let clickedCell = this;
-        let cellIdx = $(clickedCell).text();
-        console.log(cellIdx);
+        // let cellIdx = $(clickedCell).text(); // all these get functions return strings
+        // console.log(cellIdx);
+
+        let rid = $(clickedCell).attr("rid");
+        let cid = $(clickedCell).attr("cid");
+
+        let row = Number(rid) + 1;
+        let col = String.fromCharCode(Number(cid) + 65);
+
+        let address = col + row;
+        console.log(address);
+
+        $("#address").val(address);
     })
 })
